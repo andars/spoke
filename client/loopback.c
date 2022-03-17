992f304eda46a79fc0c1a3d228bba509a4ffd486
andrew <afoote97@gmail.com>
2022-03-17 00:55:59 -0700

    extend loopback client a bit
    start with 16 byte loopback, with only 1 byte in flight at a time
    
    then do 256B burst
    
    then do 1024B burst

--- client/loopback.c
+++ client/loopback.c
@@ -9,6 +9,16 @@
 int main(int argc, char *argv[]) {
     serial_open(FILENAME);
 
+    for (int i = 1; i < 16; i++) {
+        uint8_t c = i;
+        printf("sending 0x%x\n", c);
+        sendbyte(c);
+        uint8_t r = recvbyte();
+        printf("received 0x%x\n", r);
+    }
+    printf("---\n");
+
+
     for (int i = 0; i < 256; i++) {
         uint8_t c = i;
         printf("sending 0x%x\n", c);
@@ -22,4 +32,16 @@ int main(int argc, char *argv[]) {
         ASSERT_EQ(c, i);
     }
     printf("---\n");
+    for (int i = 0; i < 1024; i++) {
+        uint8_t c = i;
+        printf("sending 0x%x\n", c);
+        sendbyte(c);
+    }
+    printf("---\n");
+
+    for (int i = 0; i < 1024;i++) {
+        uint8_t c = recvbyte();
+        printf("received 0x%x\n", c);
+        ASSERT_EQ(c, (uint8_t)i);
+    }
 }
